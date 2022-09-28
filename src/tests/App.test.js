1e3619b0c3a94d3cf12bb8e6d626ca2ffd157bbe
Paulo Victor <paulo.v.prado@hotmail.com>
2022-09-28 09:33:06 -0300

    req 5 de teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('', () => {
+  it('Testa os elementos na tela inicial', () => {
+    render(<App />);
+    const title = screen.getByRole('heading', { level: 1, name:/Planets/i });
+    expect(title).toBeInTheDocument();
+
+    const inputSearch = screen.getByPlaceholderText(/Buscar/i);
+    expect(inputSearch).toBeInTheDocument();
+
+    const btnFilter = screen.getByRole('button', { name: /filtrar/i });
+    expect(btnFilter).toBeInTheDocument();
+  });
+
+  it('Testa a filtragem do maior que', () => {
+    render(<App />)
+
+    const btnFilter = screen.getByRole('button', { name: /filtrar/i });
+    userEvent.click(btnFilter);
+    const spanText = screen.getByText(/population maior que 0/i)
+  })
+})
+
